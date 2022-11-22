cfb3d0d6a6fe9d8abba854a0f26d6d89df3286bb
Luke Tutton <tuttonluke@gmail.com>
2022-11-22 18:53:01 +0000

    Adds search method.

--- web_scraper.py
+++ web_scraper.py
@@ -4,9 +4,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.keys import Keys
 import time
 #%%
 class WaterstonesScraper:
+    """_summary_
+    """
     def __init__(self) -> None:
         self.driver = webdriver.Edge()
 
@@ -29,7 +32,18 @@ class WaterstonesScraper:
             print('Loading took too long.')
         
         return self.driver
+    
+    def search(self, query) -> webdriver.Edge:
+        search_bar = self.driver.find_element(by=By.XPATH, value="//input[@class='input input-search']")
+        search_bar.click()
+        search_bar.send_keys(query)
+        search_bar.send_keys(Keys.RETURN)
+
+        return self.driver
         
 #%%
-driver = WaterstonesScraper()
-driver.load_and_accept_cookies()
+
+if __name__ == "__main__":
+    driver = WaterstonesScraper()
+    driver.load_and_accept_cookies()
+    driver.search("José Saramago")
