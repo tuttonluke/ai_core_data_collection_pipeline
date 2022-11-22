91b2bf5099f4d97ce53dc26576b2bfc5d2be1817
Luke Tutton <tuttonluke@gmail.com>
2022-11-22 18:25:54 +0000

    Adds wait for element feature for cookie acceptance.

--- web_scraper.py
+++ web_scraper.py
@@ -1,11 +1,14 @@
 #%%
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 import time
 #%%
-class Scraper:
+class WaterstonesScraper:
     def __init__(self) -> None:
-        pass
+        self.driver = webdriver.Edge()
 
     def load_and_accept_cookies(self) -> webdriver.Edge:
         """Opens Waterstones website and accepts cookies.
@@ -15,14 +18,18 @@ class Scraper:
         webdriver.Edge
             This driver is already in the Waterstones webpage.
         """
-        driver = webdriver.Edge()
         URL = "https://www.waterstones.com/"
-        driver.get(URL)
-        accept_cookies_button = driver.find_element(by=By.XPATH, value="//button[@id='onetrust-accept-btn-handler']")
-        time.sleep(2) # wait 2 seconds so the website doesn't think you're a bot
-        accept_cookies_button.click()
-
-        return driver
+        delay = 10
+        self.driver.get(URL)
+        try:
+            WebDriverWait(self.driver, delay).until(EC.presence_of_element_located((By.XPATH, "//*[@id='onetrust-banner-sdk']")))
+            accept_cookies_button = self.driver.find_element(by=By.XPATH, value="//button[@id='onetrust-accept-btn-handler']")
+            accept_cookies_button.click()
+        except TimeoutException:
+            print('Loading took too long.')
+        
+        return self.driver
+        
 #%%
-driver = Scraper()
+driver = WaterstonesScraper()
 driver.load_and_accept_cookies()
