ae28a7f3f4555e9e0793cb4471b57909cdd98e59
Luke Tutton <tuttonluke@gmail.com>
2022-11-25 10:58:06 +0000

    Minor refactoring.

--- waterstones_scraper.py
+++ waterstones_scraper.py
@@ -1,12 +1,12 @@
 #%%
+from selenium.common.exceptions import TimeoutException
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.keys import Keys
-import time
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 import pandas as pd
+import time
 import os
 import requests
 #%%
