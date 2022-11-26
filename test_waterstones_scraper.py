47efdfc6e242fdf614053c2b85273ba1a4906ffb
Luke Tutton <tuttonluke@gmail.com>
2022-11-26 10:10:16 +0000

    Adds test file test_waterstones_scraper.py.

---
+++ test_waterstones_scraper.py
@@ -0,0 +1,11 @@
+#%%
+import waterstones_scraper
+import unittest
+#%%
+class WaterstonesScraperTestCase(unittest.TestCase):
+    
+    pass
+
+
+#%%
+unittest.main(argv=['first-arg-is-ignored'], exit=False)
