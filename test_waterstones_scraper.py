922951d37efff355a2e13cbe47a870a923155052
Luke Tutton <tuttonluke@gmail.com>
2022-12-01 11:01:08 +0000

    Adds test_IDs method.

--- test_waterstones_scraper.py
+++ test_waterstones_scraper.py
@@ -1,11 +1,23 @@
 #%%
-import waterstones_scraper
+from project_files.waterstones_scraper import WaterstonesScraper
 import unittest
+import pandas as pd
+from pandas.testing import assert_series_equal
+
 #%%
 class WaterstonesScraperTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.test_driver = WaterstonesScraper('Jose Saramago')
+        return super().setUp()
     
-    pass
+    def test_IDs(self):
+        expected = pd.Series([9780099573586, 9780099461654, 9781784871789]).astype(str).rename("ID")
+        actual = self.test_driver.get_book_data()["ID"]
+        assert_series_equal(expected, actual)
+    
+
 
 
 #%%
-unittest.main(argv=['first-arg-is-ignored'], exit=False)
+unittest.main(argv=[''], exit=False)
+# %%
