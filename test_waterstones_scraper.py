a6ce80cff65fc8bfcb1300860947c8677e775432
Luke Tutton <tuttonluke@gmail.com>
2022-12-01 11:34:57 +0000

    Adds test_dtypes method.

--- test_waterstones_scraper.py
+++ test_waterstones_scraper.py
@@ -1,11 +1,12 @@
 #%%
 from pandas.testing import assert_series_equal
 from project_files.waterstones_scraper import WaterstonesScraper
+from unittest import TestCase
 import pandas as pd
 import unittest
 
 #%%
-class WaterstonesScraperTestCase(unittest.TestCase):
+class WaterstonesScraperTestCase(TestCase):
     
     @classmethod
     def setUpClass(cls) -> None:
@@ -16,13 +17,15 @@ class WaterstonesScraperTestCase(unittest.TestCase):
         self.df = self.test_driver.get_book_data()
         return super().setUp()
     
+    # @unittest.skip('Skip')
     def test_IDs(self):
         expected = pd.Series([9780099573586, 9780099461654, 9781784871789]).astype(str).rename("ID")
         actual = self.df["ID"]
         assert_series_equal(expected, actual)
     
     def test_dtypes(self):
-        pass
+        assert all(self.df.dtypes)
+        #TestCase.assertEqual(self.df.dtypes, object)
 
 
 
