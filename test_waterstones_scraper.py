1cacc0319ca44675fc19b8e69d4e6c1885bb5792
Luke Tutton <tuttonluke@gmail.com>
2022-12-01 11:08:59 +0000

    Adds setUpClass class method.

--- test_waterstones_scraper.py
+++ test_waterstones_scraper.py
@@ -1,20 +1,28 @@
 #%%
-from project_files.waterstones_scraper import WaterstonesScraper
-import unittest
-import pandas as pd
 from pandas.testing import assert_series_equal
+from project_files.waterstones_scraper import WaterstonesScraper
+import pandas as pd
+import unittest
 
 #%%
 class WaterstonesScraperTestCase(unittest.TestCase):
+    
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.test_driver = WaterstonesScraper('Jose Saramago')
+        return super().setUpClass()
+    
     def setUp(self) -> None:
-        self.test_driver = WaterstonesScraper('Jose Saramago')
+        self.df = self.test_driver.get_book_data()
         return super().setUp()
     
     def test_IDs(self):
         expected = pd.Series([9780099573586, 9780099461654, 9781784871789]).astype(str).rename("ID")
-        actual = self.test_driver.get_book_data()["ID"]
+        actual = self.df["ID"]
         assert_series_equal(expected, actual)
     
+    def test_dtypes(self):
+        pass
 
 
 
