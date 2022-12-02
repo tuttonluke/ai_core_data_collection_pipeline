aec1ebaa2e7cd28f4a273ccf43b7b817b2a11fd8
Luke Tutton <tuttonluke@gmail.com>
2022-12-02 16:24:19 +0000

    Modifies get_all_book_links_from_page.

--- project_files/waterstones_query.py
+++ project_files/waterstones_query.py
@@ -16,7 +16,7 @@ class QueryWaterstones(WaterstonesScraper):
         
         super().__init__()
         self.query = None
-        self.dict_of_languages = {}
+        self.list_of_language_page_links = []
         self.list_of_book_links = []
         self.language_filtered_DataFrame = pd.DataFrame(columns=["ID", "Timestamp", "Author", "Title", 
             "Language", "Price (£)", "Image_link"])
@@ -40,24 +40,30 @@ class QueryWaterstones(WaterstonesScraper):
             print("Invalid query input.")
 
         return self.driver
-    
+
     def get_language_filter_page_links(self):
 
-        # click more button in language filter section
-        language_more_button_xpath = "/html/body/div[1]/div[3]/div[3]/div[1]/div[2]/div[7]/div[2]/div/a"
-        WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable((By.XPATH, 
-            language_more_button_xpath))).click()
-        # populate self.dict_of_languages
-        language_container = self.driver.find_element(by=By.XPATH, 
-            value="/html/body/div[1]/div[3]/div[3]/div[1]/div[2]/div[7]/div[2]/div")
-        language_list = language_container.find_elements(by=By.TAG_NAME, value="a")
-        for language in language_list:
-            language_name = language.text
-            language_link = language.get_attribute("href")
-            self.dict_of_languages[language_name] = language_link
-        # remove information concerned with the 'less' button
-        self.dict_of_languages.pop("Less")
+        # Find language section of the filter bar (not always in the same place!)
+        search_filters = self.driver.find_elements(by=By.XPATH, 
+            value="//div[@class='filter-header slide-trigger js-filter-trigger']") 
+        language_tag = None
+        for filter in search_filters:
+            if filter.text == "LANGUAGE":
+                language_tag = filter
+        language_container = self.driver.execute_script("""
+        return arguments[0].nextElementSibling""", language_tag)
         
+        language_list = language_container.find_elements(by=By.TAG_NAME, value="a")
+        print(len(language_list))
+        for language in language_list:
+            language_link = language.get_attribute("href")
+            self.list_of_book_links.append(language_link)
+        # remove link for "less" button if present (only present with more than 5 languages)
+        if len(self.list_of_book_links) > 6:
+            self.list_of_book_links.pop()
+
+        print(self.list_of_book_links)
+
         return self.driver
 
     def get_all_book_links_from_page(self):
@@ -91,15 +97,29 @@ class QueryWaterstones(WaterstonesScraper):
             self.language_filtered_DataFrame = pd.concat([self.language_filtered_DataFrame,
             page_df])
     
-#%%
-if __name__ == "__main__":
-    driver = QueryWaterstones()
-    driver.load_and_accept_cookies()
-    driver.search("jose saramago")
-    driver.get_language_filter_page_links()
-    driver.get_DataFrame_of_language_filtered_query_results()
+    def save_df_as_csv(self):
+        if not os.path.exists(f"{self.raw_data_path}/{self.query}"):
+            os.mkdir(f"{self.raw_data_path}/{self.query}")
+        self.language_filtered_DataFrame.to_csv(f"{self.raw_data_path}/{self.query}/{self.query}.csv")
 
+    
 #%%
+driver = QueryWaterstones()
+driver.load_and_accept_cookies()
+driver.search("jose saramago")
+driver.get_language_filter_page_links()
 
-    # driver.get_all_book_links_from_page()
-    # df = driver.create_DataFrame_of_page_data()
+driver.quit_browser()
+#  if __name__ == "__main__":
+#     try:
+#         driver = QueryWaterstones()
+#         driver.load_and_accept_cookies()
+#         driver.search("gabriel garcia marquez")
+#         driver.get_language_filter_page_links()
+#         #driver.get_DataFrame_of_language_filtered_query_results()
+#         #driver.save_df_as_csv()
+#     except Exception as e:
+#         print(e)
+#         driver.quit_browser()
+
+# %%
